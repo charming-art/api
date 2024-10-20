5bc05092dca9426021d6b037b89b3faad4bc4cba
Bairui Su <pearminipro@gmail.com>
2024-10-20 15:58:48 -0400

    Fix group transform without data (#253)

--- test/apps/spin.js
+++ test/apps/spin.js
@@ -8,8 +8,11 @@ export function spin() {
       const t = Date.now() / 1000;
       return cm.group(
         cm.background({fill: "white"}),
-        cm.circle({x: 320, y: 320, r: 120, fill: "black"}),
-        cm.circle({x: 320 + Math.cos(t) * 200, y: 320 + Math.sin(t) * 200, r: 40, fill: "black"}),
+        cm.group(
+          {x: 320, y: 320},
+          cm.circle({x: 0, y: 0, r: 120, fill: "black"}),
+          cm.circle({x: Math.cos(t) * 200, y: Math.sin(t) * 200, r: 40, fill: "black"}),
+        ),
       );
     },
   });
