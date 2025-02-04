abed9ca331738ca45a7fcfc0ddef2f2e905b13ea
Bairui Su <subairui@icloud.com>
2025-02-04 15:06:21 -0500

    Add flow.on for events (#272)

--- src/flow.js
+++ src/flow.js
@@ -1,1 +1,19 @@
-export {reactive as flow} from "echox";
+import {reactive} from "echox";
+
+function event(state, key, callback, options) {
+  const listener = (event) => callback(event, state);
+  window.addEventListener(key, listener, options);
+  return () => window.removeEventListener(key, listener);
+}
+
+function flow_on(key, callback, options) {
+  return this.observe((state) => {
+    return event(state, key, callback, options);
+  });
+}
+
+export function flow() {
+  const rx = reactive();
+  rx.constructor.prototype.on ??= flow_on;
+  return rx;
+}
