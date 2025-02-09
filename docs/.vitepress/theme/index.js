f4ad2537dd6ff8ceffe6e9b1ce850e8a2083ac89
Bairui Su <subairui@icloud.com>
2025-02-09 16:58:32 -0500

    Docs for what is charming (#281)

--- docs/.vitepress/theme/index.js
+++ docs/.vitepress/theme/index.js
@@ -4,19 +4,38 @@ import {h} from "vue";
 import * as cm from "../../../src/index.js";
 import "./custom.css";
 
+const extended = {
+  random(min, max) {
+    return Math.random() * (max - min) + min;
+  },
+  constrain(x, min, max) {
+    return Math.min(Math.max(x, min), max);
+  },
+};
+
+const moduleName = (name) => {
+  return (code) => {
+    let newCode = code.replace("import {", "const {").replace(`from "charmingjs"`, "= cm");
+    newCode += `return ${name};`;
+    return `${name} = (() => {${newCode}})()`;
+  };
+};
+
 // More props: https://genji-md.dev/reference/props
 const props = {
   Theme: DefaultTheme,
-  library: {cm, svg: cm.svg, html: cm.html},
+  library: {cm: {...cm, ...extended}, svg: cm.svg, html: cm.html},
   transform: {
     module(code) {
       let newCode = code
         .replace("import {", "const {")
         .replace(`from "charmingjs"`, "= cm")
         .replace(`document.body.append(node);`, "return node;");
-      if (!newCode.includes("return node;")) newCode += "return node;";
       return `(() => {${newCode}})()`;
     },
+    moduleWalker: moduleName("walker"),
+    moduleUseWalker: moduleName("useWalker"),
+    moduleSquareWalker: moduleName("squareWalker"),
     replayable(code) {
       return `(() => {
         play;
