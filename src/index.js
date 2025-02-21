3123b220ebdc4d7bc7a982ebad459e17aa95b7e3
Bairui Su <subairui@icloud.com>
2025-02-21 13:33:49 -0500

    Remove d3-transition (#298)

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 export {render} from "./render.js";
 export {SVG, HTML} from "./collection.js";
-export {transition} from "./transition.js";
 export {constrain} from "./math.js";
 export * from "charmingjs-vector";
