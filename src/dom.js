c5c6205750605cfc79518d81ae3631917991488a
Bairui Su <subairui@icloud.com>
2025-02-20 20:10:45 -0500

    Remove echox (#294)

--- src/dom.js
+++ src/dom.js
@@ -1,16 +1,21 @@
-import {HTML as _HTML} from "echox";
+import {set} from "./set.js";
 
 const isFunction = (v) => typeof v === "function";
 
 const isArray = Array.isArray;
 
-const _SVG = _HTML("http://www.w3.org/2000/svg");
+const createNode = (namespace, name, props, children) => {
+  const dom = namespace === null ? document.createElement(name) : document.createElementNS(namespace, name);
+  set(dom, props);
+  for (const child of children) dom.appendChild(child);
+  return dom;
+};
 
 const create = (creator, name, data, options) => {
   if (options === undefined) {
     if (isArray(data)) data = {children: data};
     const {children = [], decorators = [], ...props} = data;
-    const node = creator[name](props, children);
+    const node = creator(name, props, children);
     for (const {type, ...options} of decorators) type(node, options);
     return node;
   }
@@ -20,7 +25,7 @@ const create = (creator, name, data, options) => {
     const nodeProps = {};
     for (const [k, v] of Object.entries(props)) nodeProps[k] = isFunction(v) ? v(d, i, array) : v;
     const nodeChildren = children.map((c) => (isFunction(c) ? c(d, i, array) : c));
-    const node = creator[name](nodeProps, nodeChildren);
+    const node = creator(name, nodeProps, nodeChildren);
     for (const decorator of decorators) {
       const {type, ...decoratorProps} = isFunction(decorator) ? decorator(d, i, array) : decorator;
       type(node, decoratorProps);
@@ -29,24 +34,18 @@ const create = (creator, name, data, options) => {
   });
 };
 
-const createSVG = create.bind(null, _SVG);
-
-const createHTML = create.bind(null, _HTML);
-
+// Exclude for Observable Notebook.
+// @see https://github.com/observablehq/runtime/issues/375
 const proxy = (creator) => {
-  // Exclude for Observable Notebook.
-  // @see https://github.com/observablehq/runtime/issues/375
-  const keys = ["then", "next", "return"];
+  const excludes = ["then", "next", "return"];
   return new Proxy(Object.create(null), {
     get: (_, name) => {
-      if (keys.includes(name)) return;
-      return (data, options) => {
-        return creator(name, data, options);
-      };
+      if (excludes.includes(name)) return;
+      return (data, options) => creator(name, data, options);
     },
   });
 };
 
-export const SVG = proxy(createSVG);
+export const SVG = proxy(create.bind(null, createNode.bind(null, "http://www.w3.org/2000/svg")));
 
-export const HTML = proxy(createHTML);
+export const HTML = proxy(create.bind(null, createNode.bind(null, null)));
