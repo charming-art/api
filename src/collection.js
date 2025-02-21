cdb91e5d67dfb2736ccdc46813ed0c6681043f58
pearmini <subairui@icloud.com>
2025-02-21 13:23:56 -0500

    Update implementation

--- src/collection.js
+++ src/collection.js
@@ -1,43 +1,18 @@
-const isFunction = (v) => typeof v === "function";
-
-const isArray = Array.isArray;
-
-const createNode = (svg, name, props) => ({name, props, svg, decorators: []});
-
 class Collection {
-  constructor(vdoms, children) {
-    this._vdoms = vdoms;
+  constructor(svg, tag, data, options, children) {
+    this._svg = svg;
+    this._tag = tag;
+    this._data = data;
+    this._options = options;
     this._nodes = [];
     this._children = children;
   }
 }
 
-const create = (creator, name, data, options) => {
-  if (options === undefined) {
-    if (isArray(data)) data = {children: data};
-    const {children = [], decorators = [], ...props} = data;
-    const node = creator(name, props);
-    for (const {type, ...options} of decorators) node.decorators.push({type, props: options});
-    return new Collection([node], [children]);
-  }
-  if (isArray(options)) options = {children: options};
-  const {children = [], decorators = [], ...props} = options;
-  const n = data.length;
-  const vdoms = new Array(n);
-  const childrenList = new Array(n);
-  for (let i = 0; i < n; i++) {
-    const nodeProps = {};
-    for (const [k, v] of Object.entries(props)) nodeProps[k] = isFunction(v) ? v(data[i], i, data) : v;
-    const nodeChildren = children.flatMap((c) => (isFunction(c) ? c(data[i], i, data) : c));
-    const node = creator(name, nodeProps, nodeChildren);
-    for (const decorator of decorators) {
-      const {type, ...decoratorProps} = isFunction(decorator) ? decorator(data[i], i, data) : decorator;
-      node.decorators.push({type, props: decoratorProps});
-    }
-    vdoms[i] = node;
-    childrenList[i] = nodeChildren;
-  }
-  return new Collection(vdoms, childrenList);
+const create = (svg, name, data, options) => {
+  if (options === undefined) (options = data), (data = [0]);
+  const {children = [], ...rest} = options;
+  return new Collection(svg, name, data, rest, children);
 };
 
 // Exclude for Observable Notebook.
@@ -52,6 +27,6 @@ const proxy = (creator) => {
   });
 };
 
-export const SVG = proxy(create.bind(null, createNode.bind(null, true)));
+export const SVG = proxy(create.bind(null, true));
 
-export const HTML = proxy(create.bind(null, createNode.bind(null, false)));
+export const HTML = proxy(create.bind(null, false));
--- src/render.js
+++ src/render.js
@@ -1,22 +1,31 @@
 import {set} from "./set.js";
 import {ticker} from "./ticker.js";
 
+const isFunction = (x) => typeof x === "function";
+
 // TODO: Diffing algorithm
 function patch(node, prev, current) {
   const childList = [];
   for (const collection of current) {
-    const vdoms = collection._vdoms;
-    const nodes = collection._nodes;
+    const svg = collection._svg;
+    const tag = collection._tag;
+    const data = collection._data;
+    const options = collection._options;
     const children = collection._children;
-    for (let i = 0; i < vdoms.length; i++) {
-      const vdom = vdoms[i];
-      const {name, props, svg, decorators} = vdom;
-      const child = svg ? document.createElementNS("http://www.w3.org/2000/svg", name) : document.createElement(name);
-      set(child, props);
-      for (const {type, props} of decorators) type(child, props);
+    for (let i = 0; i < data.length; i++) {
+      const d = data[i];
+      const child = svg ? document.createElementNS("http://www.w3.org/2000/svg", tag) : document.createElement(tag);
+      const {decorators = [], ...props} = options;
+      const nodeProps = {};
+      for (const [k, v] of Object.entries(props)) nodeProps[k] = isFunction(v) ? v(d, i, data) : v;
+      const nodeChildren = children.flatMap((c) => (isFunction(c) ? c(d, i, data) : c));
+      set(child, nodeProps);
+      for (const decorator of decorators) {
+        const {type, ...decoratorProps} = isFunction(decorator) ? decorator(d, i, data) : decorator;
+        type(child, decoratorProps);
+      }
       childList.push(child);
-      nodes.push(child);
-      patch(child, null, children[i]);
+      patch(child, null, nodeChildren);
     }
   }
   node.replaceChildren(...childList);
