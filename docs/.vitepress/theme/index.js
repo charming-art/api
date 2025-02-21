e5e019b3d0999a15ba5c33cd9d8a0d93eccf55ce
pearmini <subairui@icloud.com>
2025-02-21 13:31:05 -0500

    Remove d3-transition

--- docs/.vitepress/theme/index.js
+++ docs/.vitepress/theme/index.js
@@ -1,6 +1,8 @@
 import DefaultTheme from "vitepress/theme";
 import Layout from "genji-theme-vitepress";
 import {h} from "vue";
+import {selectAll} from "d3-selection";
+import "d3-transition";
 import * as cm from "../../../src/index.js";
 import "./custom.css";
 
@@ -10,6 +12,23 @@ const extended = {
   },
   constrain(x, min, max) {
     return Math.min(Math.max(x, min), max);
+  },
+  transition(node, props) {
+    const {keyframes} = props;
+    const selection = selectAll([node]);
+    let transition = selection;
+
+    for (const {attr = {}, style = {}, duration, ease, delay} of keyframes) {
+      transition = transition.transition();
+      transition
+        .duration(duration)
+        .call((t) => ease && t.ease(ease))
+        .call((t) => delay && t.delay(delay));
+      for (const key in attr) transition.attr(key, attr[key]);
+      for (const key in style) transition.style(key, style[key]);
+    }
+
+    return node;
   },
 };
 
