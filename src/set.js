3190ca0746392732c593b7b953108396b33174e6
pearmini <subairui@icloud.com>
2025-02-21 10:21:05 -0500

    Update set.js

--- src/set.js
+++ src/set.js
@@ -74,10 +74,9 @@ const svgKeyof = (key) => (svgCamelCaseAttributes.has(key) ? key : toKebabCase(k
 // @see https://github.com/vanjs-org/van/blob/main/src/van.js
 function setAttribute(dom, k, v) {
   const name = dom.tagName.toLowerCase();
-  const getPropDescriptor = (proto) =>
-    proto ? (Object.getOwnPropertyDescriptor(proto, k) ?? getPropDescriptor(protoOf(proto))) : undefined;
+  const get = (proto) => (proto ? (Object.getOwnPropertyDescriptor(proto, k) ?? get(protoOf(proto))) : undefined);
   const cacheKey = name + "," + k;
-  const propSetter = (propSetterCache[cacheKey] ??= getPropDescriptor(protoOf(dom))?.set ?? 0);
+  const propSetter = (propSetterCache[cacheKey] ??= get(protoOf(dom))?.set ?? 0);
   const setter = propSetter ? propSetter.bind(dom) : dom.setAttribute.bind(dom, k);
   setter(v);
 }
