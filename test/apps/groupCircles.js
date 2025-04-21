08085a6e5f5e7e138e161030cf76b2580d05b748
pearmini <subairui@icloud.com>
2025-04-20 21:31:43 -0400

    Fix callback children

--- test/apps/groupCircles.js
+++ test/apps/groupCircles.js
@@ -4,7 +4,7 @@ export function groupCircles() {
   const data = Array.from({length: 5}, (_, i) => i);
   const svg = cm.svg("svg", {
     width: 30 * (data.length + 1),
-    height: 70,
+    height: 90,
     children: [
       cm.svg("g", data, {
         transform: (i) => `translate(${(i + 1) * 30}, 20)`,
@@ -13,6 +13,10 @@ export function groupCircles() {
       cm.svg("g", data, {
         transform: (i) => `translate(${(i + 1) * 30}, 50)`,
         children: [[cm.svg("circle", {r: 10, fill: "black"}), cm.svg("circle", {r: 5, fill: "white"})]],
+      }),
+      cm.svg("g", data, {
+        transform: (i) => `translate(${(i + 1) * 30}, 80)`,
+        children: (i) => [cm.svg("circle", {r: (i + 1) * 2, fill: "black"})],
       }),
     ],
   });
