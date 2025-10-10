1c15fbb9258010091686c38e82359d5f84e167d6
pearmini <subairui@icloud.com>
2025-10-10 00:00:01 -0400

    Change base

--- docs/.vitepress/config.mjs
+++ docs/.vitepress/config.mjs
@@ -7,6 +7,7 @@ export default defineConfig({
   description: "The JavaScript library for expressive creative coding",
   cleanUrls: true,
   extends: config,
+  base: "/api/",
   head: [["link", {rel: "icon", type: "image/png", href: "/logo.png"}]],
   appearance: false,
   themeConfig: {
