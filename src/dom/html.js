3b990ebc1c808ac222bd27cc1e80f2bc616ec52d
pearmini <subairui@icloud.com>
2026-04-01 15:32:23 -0400

    Change from function api to template api

--- src/dom/html.js
+++ src/dom/html.js
@@ -1,3 +1,7 @@
 import {tag} from "./tag.js";
 
-export const html = tag(null);
+export const html = tag((string) => {
+  const div = document.createElement("div");
+  div.innerHTML = string;
+  return div;
+});
