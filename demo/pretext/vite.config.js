badeffb632e137e3c90789db3a630de9ae495a72
pearmini <subairui@icloud.com>
2026-04-04 09:30:08 -0400

    Update index.html

--- demo/pretext/vite.config.js
+++ demo/pretext/vite.config.js
@@ -13,7 +13,7 @@ export default defineConfig({
       input: {
         main: resolve(__dirname, "index.html"),
         "us-map-paper": resolve(__dirname, "us-map-paper/index.html"),
-        yinyang: resolve(__dirname, "yinyang/index.html"),
+        "yin-yang": resolve(__dirname, "yin-yang/index.html"),
         "air-text": resolve(__dirname, "air-text/index.html"),
       },
     },
